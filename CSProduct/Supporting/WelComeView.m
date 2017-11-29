0ddc468352d41d7509f745d1c2ca7b3373bcfc55
zhiwei jiang <jzw@zhiweideMacBook-Pro.local>
2017-11-29 10:40:16 +0800

    增加欢迎页面

---
+++ CSProduct/Supporting/WelComeView.m
@@ -0,0 +1,21 @@
+//
+//  WelComeView.m
+//  CSProduct
+//
+//  Created by zhiwei jiang on 2017/11/29.
+//  Copyright © 2017年 zhiwei jiang. All rights reserved.
+//
+
+#import "WelComeView.h"
+
+@implementation WelComeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
